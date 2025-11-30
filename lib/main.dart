0c06ee11303273db2d19cd8aa3abec81cb165d19
Abdalrhman Ghanima <abdoghanima2005@gmail.com>
2025-11-30 05:28:56 +0200

    Fix theme provider + drawer navigation + added theme toggling

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:news/core/routes/app_router.dart';
 import 'package:news/core/routes/page_routes.dart';
+import 'package:provider/provider.dart';
+
+import 'modelus/home/view/theme_provider.dart';
 
 GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => ThemeProvider(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
       onGenerateRoute: AppRouter.generateRoute,
       initialRoute: PageRoutesName.initial,
-      debugShowCheckedModeBanner: false,
       navigatorKey: navigatorKey,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+      debugShowCheckedModeBanner: false,
+
+      themeMode: themeProvider.getThemeMode,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
     );
   }
 }
+
--- lib/modelus/home/view/custom_drawer.dart
+++ lib/modelus/home/view/custom_drawer.dart
@@ -1,43 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:news/modelus/home/view/theme_provider.dart';
+
+import '../../../core/routes/page_routes.dart';
 
 class CustomDrawer extends StatelessWidget {
   final Function()? onTap;
   const CustomDrawer({super.key, this.onTap});
 
-
-
   @override
   Widget build(BuildContext context) {
     return Drawer(
-      backgroundColor: Color(0xff171717),
       child: Column(
         children: [
-          Container(
-            color: Colors.white,
-            child: DrawerHeader(
-              child: Center(
-                child: Text(
-                  "News App",
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.w700),
-                ),
-              ),
-            ),
-          ),
+          DrawerHeader(child: Center(child: Text('News App'))),
+
           GestureDetector(
-            onTap: onTap,
+            onTap: () {
+              Navigator.pop(context);
+              Navigator.pushNamed(context, PageRoutesName.home);
+            },
             child: ListTile(
-              leading: Icon(Icons.home, color: Colors.white),
-              title: Text(
-                "Go To Home",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.w700,
-                  color: Colors.white,
-                ),
-              ),
+              title: Text('Go To Home'),
+              leading: Icon(Icons.home),
             ),
           ),
-          Divider(color: Colors.white),
+
+          Divider(),
+
+          GestureDetector(
+            onTap: () {
+              final themeProvider = Provider.of<ThemeProvider>(context, listen: false);
+
+              themeProvider.setThemeMode =
+              themeProvider.isDarkMode ? ThemeMode.light : ThemeMode.dark;
+
+              Navigator.pop(context);
+            },
+            child: ListTile(
+              title: Text('Change Theme'),
+              leading: Icon(Icons.dark_mode),
+            ),
+          ),
+
+
         ],
       ),
     );
